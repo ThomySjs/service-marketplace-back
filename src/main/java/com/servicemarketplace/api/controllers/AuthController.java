3819ab0a89af3b2fdc565202e160875a2c4f6f88
ThomySjs <Thomysejas@hotmail.com>
2025-09-02 21:16:24 -0300

    fix: Corrige la notacion del controlador para permitir las vistas.

--- src/main/java/com/servicemarketplace/api/controllers/AuthController.java
+++ src/main/java/com/servicemarketplace/api/controllers/AuthController.java
@@ -1,13 +1,13 @@
 package com.servicemarketplace.api.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.servicemarketplace.api.dto.auth.LoginRequest;
 import com.servicemarketplace.api.dto.auth.RegisterRequest;
@@ -21,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpHeaders;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
