11de419c58d9022d3dea6a4a0a17249106a70d89
ThomySjs <Thomysejas@hotmail.com>
2025-09-02 21:14:47 -0300

    fix: Corrige nombre de key

--- src/main/java/com/servicemarketplace/api/dto/auth/TokenResponse.java
+++ src/main/java/com/servicemarketplace/api/dto/auth/TokenResponse.java
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record TokenResponse(
     @JsonProperty("access_token")
     String accessToken,
-    @JsonProperty("username")
-    String username
+    @JsonProperty("refresh_token")
+    String refreshToken
 ) {}
