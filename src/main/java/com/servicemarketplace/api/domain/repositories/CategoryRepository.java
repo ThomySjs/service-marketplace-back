fd35c6d8bf7eb3a8235217c63958f35242536ab7
ThomySjs <Thomysejas@hotmail.com>
2025-09-03 23:40:57 -0300

    fix: Corrige error de importacion de la clase Category

--- src/main/java/com/servicemarketplace/api/domain/repositories/CategoryRepository.java
+++ src/main/java/com/servicemarketplace/api/domain/repositories/CategoryRepository.java
@@ -1,9 +1,9 @@
 package com.servicemarketplace.api.domain.repositories;
 
-import java.util.Locale.Category;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.servicemarketplace.api.domain.entities.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
