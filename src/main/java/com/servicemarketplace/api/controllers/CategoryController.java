60ca93e4b7e71d623d1d392c7952e9b06fa26476
ThomySjs <Thomysejas@hotmail.com>
2025-09-04 23:05:58 -0300

    Revert "feat: Agrega la funcionalidad para crear categorias."
    This reverts commit 2af8f6347e22859bc603122dec5cc078d8627496.

--- src/main/java/com/servicemarketplace/api/controllers/CategoryController.java
+++ src/main/java/com/servicemarketplace/api/controllers/CategoryController.java
@@ -2,28 +2,9 @@ package com.servicemarketplace.api.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.servicemarketplace.api.dto.CategoryRequest;
-import com.servicemarketplace.api.dto.CategoryResponse;
-import com.servicemarketplace.api.services.CategoryService;
-
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/categories")
 public class CategoryController {
-
-    private final CategoryService categoryService;
-
-    @PostMapping("/add")
-    public ResponseEntity<CategoryResponse> add(@Validated @RequestBody CategoryRequest request) {
-        return ResponseEntity.ok(categoryService.create(request));
-    }
-
 }
--- src/main/java/com/servicemarketplace/api/services/CategoryService.java
+++ src/main/java/com/servicemarketplace/api/services/CategoryService.java
@@ -1,8 +1,4 @@
 package com.servicemarketplace.api.services;
 
-import com.servicemarketplace.api.dto.CategoryRequest;
-import com.servicemarketplace.api.dto.CategoryResponse;
-
-public interface CategoryService {
-    CategoryResponse create(CategoryRequest categoryRequest);
+public class CategoryService {
 }
