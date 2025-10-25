94a294d726ceac773d05855394d5345cc33eef9f
ThomySjs <Thomysejas@hotmail.com>
2025-10-24 21:39:56 -0300

    feat: Agrega mapper necesario para el DTO de transacciones.

--- src/main/java/com/servicemarketplace/api/mappers/UserMapper.java
+++ src/main/java/com/servicemarketplace/api/mappers/UserMapper.java
@@ -3,6 +3,7 @@ package com.servicemarketplace.api.mappers;
 import com.servicemarketplace.api.domain.entities.User;
 import com.servicemarketplace.api.dto.service.ServiceListResponse;
 import com.servicemarketplace.api.dto.user.UserDTO;
+import com.servicemarketplace.api.dto.user.UserForTransactionDTO;
 
 public class UserMapper {
 
@@ -29,6 +30,14 @@ public class UserMapper {
                 s.getDescription()
             ))
         .toList())
+            .build();
+    }
+
+    public static UserForTransactionDTO toUserForTransactionDTO(User user) {
+        return UserForTransactionDTO.builder()
+            .id(user.getId())
+            .email(user.getEmail())
+            .name(user.getName())
             .build();
     }
 }
