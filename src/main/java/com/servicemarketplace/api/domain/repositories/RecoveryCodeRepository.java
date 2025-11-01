6564b43f6a5289f85af2f4057d0b5e61e7da058e
ThomySjs <Thomysejas@hotmail.com>
2025-11-01 14:13:29 -0300

    fix: Agrega anotaciones faltantes.

--- src/main/java/com/servicemarketplace/api/domain/repositories/RecoveryCodeRepository.java
+++ src/main/java/com/servicemarketplace/api/domain/repositories/RecoveryCodeRepository.java
@@ -3,9 +3,11 @@ package com.servicemarketplace.api.domain.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.servicemarketplace.api.domain.entities.RecoveryCode;
 
+@Repository
 public interface RecoveryCodeRepository extends JpaRepository<RecoveryCode, Long> {
 
     public Optional<RecoveryCode> findByEmail(String email);
