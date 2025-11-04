43d642fbbae064ca1cd3263a0425ef31caa77669
ThomySjs <Thomysejas@hotmail.com>
2025-11-04 12:58:51 -0300

    feat: Agrega limite de servicios a los roles.

--- src/main/java/com/servicemarketplace/api/config/Roles.java
+++ src/main/java/com/servicemarketplace/api/config/Roles.java
@@ -1,7 +1,17 @@
 package com.servicemarketplace.api.config;
 
 public enum Roles {
-    USER,
-    ADMIN,
-    PREMIUM
+    USER(1L),
+    ADMIN(1000L),
+    PREMIUM(3L);
+
+    private final Long limit;
+
+    Roles(Long limit) {
+        this.limit = limit;
+    }
+
+    public Long getServiceLimit() {
+        return this.limit;
+    }
 }
