edb5925ccb6cb53d8cffcafadfe8908a42b1b061
ThomySjs <Thomysejas@hotmail.com>
2025-11-14 10:53:14 -0300

    feat: Agrega endpoint para obtener las causas de rechazo por paginas (para panel de administrador)

--- src/main/java/com/servicemarketplace/api/controllers/ServiceRejectCauseController.java
+++ src/main/java/com/servicemarketplace/api/controllers/ServiceRejectCauseController.java
@@ -2,12 +2,22 @@ package com.servicemarketplace.api.controllers;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.servicemarketplace.api.domain.entities.ServiceRejectCause;
+import com.servicemarketplace.api.dto.RejectCauseDTO;
 import com.servicemarketplace.api.dto.ServiceRejectCauseDTO;
 import com.servicemarketplace.api.services.ServiceRejectCauseService;
 
@@ -26,6 +36,29 @@ public class ServiceRejectCauseController {
     @GetMapping
     public ResponseEntity<List<ServiceRejectCauseDTO>> getAll() {
         return ResponseEntity.ok(service.getAll());
+    }
+
+    @Operation(summary = "Obtiene todas las causas de rechazo de servicio.")
+	@PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/paginated")
+    public ResponseEntity<Page<ServiceRejectCause>> getAllPaginated(Pageable pageable) {
+        return ResponseEntity.ok(service.getAllPaginated(pageable));
+    }
+
+    @Operation(summary = "Agrega un motivo de rechazo.")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> create(@Validated @RequestBody RejectCauseDTO request) {
+        service.create(request.getMessage());
+        return ResponseEntity.ok("Servicio creado correctamente");
+    }
+
+    @Operation(summary = "Elimina un motivo.")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long causeId) {
+        service.delete(causeId);
+        return ResponseEntity.ok("Servicio eliminado correctamente");
     }
 }
 
--- src/main/java/com/servicemarketplace/api/services/impl/ServiceRejectCauseServiceImpl.java
+++ src/main/java/com/servicemarketplace/api/services/impl/ServiceRejectCauseServiceImpl.java
@@ -3,8 +3,11 @@ package com.servicemarketplace.api.services.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.servicemarketplace.api.domain.entities.ServiceRejectCause;
 import com.servicemarketplace.api.domain.repositories.ServiceRejectCauseRepository;
 import com.servicemarketplace.api.dto.ServiceRejectCauseDTO;
 import com.servicemarketplace.api.services.ServiceRejectCauseService;
@@ -23,6 +26,23 @@ public class ServiceRejectCauseServiceImpl implements ServiceRejectCauseService 
                 .stream()
                 .map(c -> new ServiceRejectCauseDTO(c.getId(), c.getMessage()))
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public Page<ServiceRejectCause> getAllPaginated(Pageable pageable) {
+        return repository.findAllPaginated(pageable);
+    }
+
+    @Override
+    public void create(String message) {
+        repository.save(ServiceRejectCause.builder()
+            .message(message)
+            .build()
+        );
+    }
+
+    public void delete(Long causeId){
+        repository.deleteById(causeId);
     }
 }
 
